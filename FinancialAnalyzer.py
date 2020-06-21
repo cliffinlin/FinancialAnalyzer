f9db177a5128710ef07e9ec5cea71cecba6255cb
cliffinlin <13520500645@139.com>
2020-06-21 19:18:15 +0800

    update

--- FinancialAnalyzer.py
+++ FinancialAnalyzer.py
@@ -10,10 +10,10 @@ import Financial
 
 
 def main():
-    # Financial.setup_database()
-    # Financial.download_stock_list()
-    # Financial.download()
-    # Financial.analyze()
+    Financial.setup_database()
+    Financial.download_stock_list()
+    Financial.download()
+    Financial.analyze()
     # Financial.update_mark()
     Draw.draw("roe >= 15 AND pe > 10", "pe", "DESC")
     # Draw.draw("roe >= 15 AND pe > 10", "roe", "DESC")
